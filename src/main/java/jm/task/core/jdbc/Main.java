76469e4806f291cbd413b8c2ce5226dae87ec555
ClassIC52 <79601814057@bk.ru>
2024-09-06 14:39:16 +0300

    Task 1.1.3 JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Алексей", "Кудряшов",(byte) 24);
+        us.saveUser("Дмитрий", "Шаповалов",(byte) 27);
+        us.saveUser("Дмитрий", "Беляков",(byte) 29);
+        us.saveUser("Михаил", "Латыпов",(byte) 39);
+        List<User> list = us.getAllUsers();
+        for (User user : list) {
+            System.out.println(user);
+        }
+        us.removeUserById(3);
+        us.cleanUsersTable();
+        us.dropUsersTable();
     }
 }
